db5edd3df2f980328ba88261d465a6886e63249d
kingrex0830 <ycea0830@yahoo.com>
2023-03-23 11:07:39 -0400

    Finished javascript, added comments, filled in ReadME

--- Assets/script.js
+++ Assets/script.js
@@ -2,25 +2,15 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.getElementById("#generate");
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var specchar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "`", "~", "[", "]", "{", "}", "|", ",", "<", ">", ".", "?", "/", " "];
-
+var generateBtn = document.querySelector("#generate");
+// This is where we generate the password
 function generatePassword() {
-
-  var userOptions = {
-    length: "",
-    useLowercase: null,
-    useUppercase: null,
-    useNumbers: null,
-    useSymbols: null
-  }
-
+  // Set default length of the password, will be changed by the user later
+  var passLength= "";
+  // User input for changing the length of the password
   var askLength = window.prompt("How long would you like your password? Choose between 8 and 128 characters.");
   Number(askLength);
-
+  // Rejects user input if input is greater than 128 or less than 8. If user input is between these numbers, it becomes the new password length
   if (askLength < 8) {
     alert("Please pick a longer character limit.");
     return;
@@ -28,14 +18,43 @@ function generatePassword() {
     alert("Please pick a smaller character limit.");
     return;
   } else if (askLength <= 128 && askLength >= 8) {
-    userOptions.length = askLength;
+    passLength = askLength;
   } else {
     return;
   }
+  // Sets characters and password as empty variables to be filled in later
+  var characters = [];
+  var password = "";
+  // Asks user what kinds of characters they want in their password
+  var useLowercase = confirm("Would you like lowercase letters in your password?");
+  var useUppercase = confirm("Would you like uppercase letters in your password?");
+  var useNum = confirm("Would you like numbers in your password?");
+  var useSpecChar = confirm("Would you like special characters in your password?");
+  // Pushes the appropriate characters onto the characters variables based on user input
+  if (useLowercase) {
+    characters.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+  }
+  if (useUppercase) {
+    characters.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+  }
+  if (useNum) {
+    characters.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+  }
+  if (useSpecChar) {
+    characters.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "`", "~", "[", "]", "{", "}", "|", ",", "<", ">", ".", "?", "/", " ");
+  } else if (!useLowercase && !useUppercase && !useNum && !useSpecChar) {
+    alert("Please choose at least one character set!")
+  }
+  // Uses a for loop to create a string made from random parts of the characters array, then assigns that value to the password variable
+  for (i = 0 ; i < passLength ; i++) {
+    var randomChar = characters[Math.floor(Math.random() * characters.length)];
+    password = password + randomChar;
+  }
+  // Returns the new password for the user
+  return password;
 
 }
-
-// Write password to the #password input
+// Puts the generated password in the HTML textbox
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -43,6 +62,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
+// Runs the writePassword function on clicking the button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Password Generator Starter Code
+# <Module 3 Project - Password Generator>
+
+## Description
+
+This application was designed to take input from a user and generate for them a random password of 8-128 characters, using lowercase letters, uppercase letters, special characters, and numbers, all depending on user input.
+
+## Installation
+
+Simply follow this link to view the web page: 
+
+## Usage
+
+When you click the Generate Password button, a series of prompts will ask you about your criteria for a password, such as length or what characters will be included. Once finished, it will generate a random stirng of characters based on this input and print it to the text box.
+
+## Credits
+
+Received assistance from Julie Macpherson and AskBCS for bug fixing and utilizing the code more effectively.
