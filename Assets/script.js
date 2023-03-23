f6fa0ab3070f32f57ded499112f23951f0c0ec4b
kingrex0830 <ycea0830@yahoo.com>
2023-03-23 09:40:43 -0400

    Added more javascript to project

--- Assets/script.js
+++ Assets/script.js
@@ -2,19 +2,43 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("#generate");
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specchar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "`", "~", "[", "]", "{", "}", "|", ",", "<", ">", ".", "?", "/", " "];
+
+function generatePassword() {
+
+  var userOptions = {
+    length: "",
+    useLowercase: null,
+    useUppercase: null,
+    useNumbers: null,
+    useSymbols: null
+  }
+
+  var askLength = window.prompt("How long would you like your password? Choose between 8 and 128 characters.");
+  Number(askLength);
+
+  if (askLength < 8) {
+    alert("Please pick a longer character limit.");
+    return;
+  } else if (askLength > 128) {
+    alert("Please pick a smaller character limit.");
+    return;
+  } else if (askLength <= 128 && askLength >= 8) {
+    userOptions.length = askLength;
+  } else {
+    return;
+  }
+
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var limitInput = document.getElementByID("num-sel");
-  var result = document.getElementByID("textarea");
-
-  for (let i=0; i < limitInput; i++) {
-      
-  }
-
 
   passwordText.value = password;
 
